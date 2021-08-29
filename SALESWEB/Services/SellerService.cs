026152bf81a5c6834f528b471e2e4e631cd85c54
LeoPazEs <leopestevam@hotmail.com>
2021-08-29 08:37:49 -0300

    Seller Service

---
+++ SALESWEB/Services/SellerService.cs
@@ -0,0 +1,24 @@
+﻿using SALESWEB.Data;
+using SALESWEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SALESWEB.Services
+{
+    public class SellerService
+    {
+        private readonly SALESWEBContext _context; 
+
+        public SellerService(SALESWEBContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
--- SALESWEB/Startup.cs
+++ SALESWEB/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SALESWEB.Data;
+using SALESWEB.Services;
 
 namespace SALESWEB
 {
@@ -41,6 +42,7 @@ namespace SALESWEB
                     builder.MigrationsAssembly("SALESWEB")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
